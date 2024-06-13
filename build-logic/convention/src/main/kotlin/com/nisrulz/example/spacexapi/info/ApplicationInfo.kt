bc36ea68c08e129ea56d27b1592155a836f32f42
Nishant Srivastava <nisrulz@gmail.com>
2024-06-14 01:09:11 +0200

    added info classes

---
+++ build-logic/convention/src/main/kotlin/com/nisrulz/example/spacexapi/info/ApplicationInfo.kt
@@ -0,0 +1,7 @@
+package com.nisrulz.example.spacexapi.info
+
+object ApplicationInfo {
+    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 1
+    const val BASE_NAMESPACE = "com.nisrulz.example.spacexapi"
+}
---
+++ build-logic/convention/src/main/kotlin/com/nisrulz/example/spacexapi/info/BuildSdkInfo.kt
@@ -0,0 +1,7 @@
+package com.nisrulz.example.spacexapi.info
+
+object BuildSdkInfo {
+    const val COMPILE_SDK_VERSION = 34
+    const val MIN_SDK_VERSION = 21
+    const val JVM_TARGET = 17
+}
