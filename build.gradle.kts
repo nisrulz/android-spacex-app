256062f112f8c1689d69e5fbb99c5de19eadb43f
Nishant Srivastava <nisrulz@gmail.com>
2024-06-14 01:08:04 +0200

    cleanup code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Android
     alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.android.library) apply false
 
+    // Kotlin
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+
+    // Hilt
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
 
+    // Serialization
     alias(libs.plugins.kotlin.serialization) apply false
+
+    // Compose
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-true // Needed to make the Suppress annotation work for the plugins block
