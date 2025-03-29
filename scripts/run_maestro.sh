2e1081803acbd2577156372e153a6ea68b899888
Nishant Srivastava <nisrulz@gmail.com>
2025-03-29 02:27:00 +0100

    modify github actions to test comment back with results

--- scripts/run_maestro.sh
+++ scripts/run_maestro.sh
@@ -4,4 +4,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 # Run Maestro Tests
 export APP_ID="com.nisrulz.example.spacexapi"
-maestro test -e APP_ID=$APP_ID .maestro/
+maestro test -e APP_ID=$APP_ID .maestro/ --format junit
+
+# Process results
+python3 ./scripts/process_maestro_results.py
