cb2159bd214f263f3faa58b71bcedd978ccf0433
Nishant Srivastava <nisrulz@gmail.com>
2025-03-29 03:01:17 +0100

    updated scripts

--- scripts/run_maestro.sh
+++ scripts/run_maestro.sh
@@ -3,7 +3,12 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
 # Run Maestro Tests
+export MAESTRO_CLI_ANALYSIS_NOTIFICATION_DISABLED=true
+export MAESTRO_CLI_NO_ANALYTICS=1
+export PATH="$PATH":"$HOME/.maestro/bin"
+
 export APP_ID="com.nisrulz.example.spacexapi"
+
 maestro test -e APP_ID=$APP_ID .maestro/ --format junit
 
 # Process results
--- scripts/setup_maestro.sh
+++ scripts/setup_maestro.sh
@@ -4,6 +4,3 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 # Install Maestro CLI
 curl -Ls "https://get.maestro.mobile.dev" | bash
-export PATH="$HOME/.maestro/bin:$PATH"
-export MAESTRO_CLI_ANALYSIS_NOTIFICATION_DISABLED=true
-export MAESTRO_CLI_NO_ANALYTICS=1
